e308133ff8799f98c6b504c82c489639a71c8f46
Jesus David Villota <jesus.villotaa@campusucc.edu.co>
2025-11-20 01:09:31 -0500

    Auto-crea superusuario en Render

--- scripts/render_start.sh
+++ scripts/render_start.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 echo "[render_start] running migrations"
 python manage.py migrate --no-input
 
+echo "[render_start] Creando superusuario si no existe..."
+python manage.py createsuperuser --noinput || true
+
 echo "[render_start] Arrancando Gunicorn..."
 exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-10000} --workers 3
