2e30e88106aab0bfddf0fc75d4f4c01a224ceb07
ghosstbabby12 <camila.bastidas@campusucc.edu.co>
2025-11-20 21:17:23 -0500

    Added automatic admin creation

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -6,7 +6,7 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         try:
-            from apps.core.create_admin import create_default_admin
+            from apps.core.create_default_admin import create_default_admin
             create_default_admin()
         except Exception as e:
             print(f"⚠️ Error creating default admin: {e}")
